b41e99f265a432e0c50c2311609490c5d45d850f
TT-ghost_semir <102698694+TTghostsemir@users.noreply.github.com>
2023-03-22 05:18:53 +0100

    Übersetzte err.c auf Deutsch

--- source/err.c
+++ source/err.c
@@ -18,19 +18,19 @@ const char *TEErrors[] = {
     "NO MEM",
     "NO FAT",
     "MKFS ABORT",
-    [TE_ERR_UNIMPLEMENTED - 1] = "Unimplemented",
-    [TE_EXCEPTION_RESET - 1] = "E Reset",
-    [TE_EXCEPTION_UNDEFINED - 1] = "E Undefined",
-    [TE_EXCEPTION_PREF_ABORT - 1] = "E Pref abort",
-    [TE_EXCEPTION_DATA_ABORT - 1] = "E Data abort",
-    [TE_ERR_SAME_LOC - 1] = "Same copy location",
-    [TE_ERR_KEYDUMP_FAIL - 1] = "Keydump failed",
-    [TE_ERR_PARTITION_NOT_FOUND - 1] = "Failed to find partition",
-    [TE_ERR_PATH_IN_PATH - 1] = "Can't move/copy folder into itself",
-    [TE_ERR_EMMC_READ_FAIL - 1] = "Emmc/Emummc read failed",
-    [TE_ERR_EMMC_WRITE_FAIL - 1] = "Emmc/Emummc write failed",
-    [TE_ERR_NO_SD - 1] = "No sd detected",
-    [TE_ERR_FILE_TOO_BIG_FOR_DEST - 1] = "File is too big for dest",
+    [TE_ERR_UNIMPLEMENTED - 1] = "Nicht Implementiert",
+    [TE_EXCEPTION_RESET - 1] = "E Zuruecksetzen",
+    [TE_EXCEPTION_UNDEFINED - 1] = "E Unbestimmt",
+    [TE_EXCEPTION_PREF_ABORT - 1] = "E Pref Ueber",
+    [TE_EXCEPTION_DATA_ABORT - 1] = "E Daten ueber",
+    [TE_ERR_SAME_LOC - 1] = "Selber Kopier Ort",
+    [TE_ERR_KEYDUMP_FAIL - 1] = "Keysicherung fehlgeschlagen",
+    [TE_ERR_PARTITION_NOT_FOUND - 1] = "Partition nicht gefunden",
+    [TE_ERR_PATH_IN_PATH - 1] = "Ordner kann nicht in sich selbst verschoben/kopiert werden",
+    [TE_ERR_EMMC_READ_FAIL - 1] = "eMMC/emuMMC fehler beim Lesen",
+    [TE_ERR_EMMC_WRITE_FAIL - 1] = "eMMC/emuMMC fehler beim Schreiben",
+    [TE_ERR_NO_SD - 1] = "Keine SD gefunden",
+    [TE_ERR_FILE_TOO_BIG_FOR_DEST - 1] = "Datei ist zu gross fuer das ziel",
 };
 
 const char *GetErrStr(u32 err){
@@ -54,9 +54,9 @@ void DrawError(ErrCode_t err){
     gfx_box(lx, ly, lx + lenx, ly + leny, COLOR_ORANGE);
     gfx_boxGrey(lx + 16, ly + 16, lx + lenx - 16, ly + leny - 16, 0x33);
     gfx_con_setpos(lx + ((lenx - 17 * 16) / 2), ly + 32);
-    gfx_printf("An error occured!\n\n%bErr : %d\nLine: %d\nFile: %s\nDesc: %s%b", lx + 48, err.err, err.loc, err.file, GetErrStr(err.err), 0);
+    gfx_printf("Ein Fehler ist aufgetaucht!\n\n%bErr : %d\nLine: %d\nFile: %s\nDesc: %s%b", lx + 48, err.err, err.loc, err.file, GetErrStr(err.err), 0);
     gfx_con_setpos(lx + ((lenx - 19 * 16) / 2), ly + leny - 48);
-    gfx_printf("Press A to continue");
+    gfx_printf("Druecke A um Fortzufahren");
     
     hidWaitMask((JoyA | JoyB));
 }
